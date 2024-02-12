3cead24becc1138f721834452ee4320c1b03a0d6
Carlos Russell <russe536@msu.edu>
2024-02-12 12:31:38 -0500

    distances and planets config

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,14 @@
 // The export statement makes these settings available to other files in 11ty
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
-  eleventyConfig.addPassthroughCopy("styles");
+    eleventyConfig.addPassthroughCopy("styles");
+
+    eleventyConfig.addShortcode("Planets", Function (Name, Name2, AU) {
+        return `<div class="planet">
+    <div class="circle ${name2}"></div>
+    <h1 class="name">${name}</h1>
+    <p>${AU}</p>
+  </div>`
+    })
+
 };
