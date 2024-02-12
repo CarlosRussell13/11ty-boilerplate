8cffbcd79e6165f3c643f1777e71d8e3484fca40
CarlosRussell13 <133693000+CarlosRussell13@users.noreply.github.com>
2024-02-12 22:30:04 +0000

    maybe

--- .eleventy.js
+++ .eleventy.js
@@ -7,9 +7,9 @@ module.exports = function(eleventyConfig) {
 
       return `<div class="planet">
 
-      <div class="circle ${name}"></div>
+      <div class="${name} circle"></div>
 
-      <h2 class="name">${NameofPlanet}</h2>
+      <h2 class="name">${nameofplanet}</h2>
 
       <p>${AU}</p>
 
