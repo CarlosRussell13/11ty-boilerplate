731fa722ef8ec8897aa23da613394b1ce86e2f69
CarlosRussell13 <133693000+CarlosRussell13@users.noreply.github.com>
2024-02-13 21:51:40 +0000

    last time fixing it

--- .eleventy.js
+++ .eleventy.js
@@ -2,19 +2,4 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("styles");
-    
-    eleventyConfig.addShortcode("Planets", function(name, nameofplanet, AU) {
-
-      return `<div class="planet">
-
-      <div class="${name} circle"></div>
-
-      <h2 class="name">${nameofplanet}</h2>
-
-      <p>${AU}</p>
-
-    </div>`
-
-    })
-
-  };
+};
