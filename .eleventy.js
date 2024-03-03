439177398b5425d6cc423060b98459a39a8e4fbb
CarlosRussell13 <133693000+CarlosRussell13@users.noreply.github.com>
2024-03-03 13:49:09 +0000

    alpinejs installation and button creation

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("styles");
+    eleventyConfig.addPassthroughCopy("node_modules");
+  eleventyConfig.addPassthroughCopy("alpine.js");
 };
